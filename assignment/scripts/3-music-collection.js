7fb7ba0646ca169c154c175490251bf33394fe0a
Scott Schulte <scott.r.schulte@gmail.com>
2024-04-16 15:19:13 -0500

    findByArtist function is almost complete. Comments included about what is wrong. Will come back to it after letting it sit for a little while.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,7 +2,6 @@ console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 let myCollection = [];
 
-//Not passing test
 function addToCollection(collection, title, artist, yearPublished) {
   const record = {
     collection,
@@ -19,15 +18,33 @@ function showCollection(collection) {
   }
 }
 
+// Will not show all albums by specific artist and says artists that are in collection are not.
+let albumArray = [];
+function findByArtist(collection, artist) {
+  for (let x = 0; x < collection.length; x++){
+    if (artist === collection[x].artist) {
+      albumArray.push(collection[x]);
+      return albumArray;
+    } else {
+      return `${artist} not in collection`;
+    }
+  }
+}
+
+
 console.log(addToCollection(myCollection, 'Nevermind', 'Nirvana', 1991));
 console.log(addToCollection(myCollection, 'Ten', 'Pearl Jam', 1991));
 console.log(addToCollection(myCollection, 'The Chronic', 'Dr. Dre', 1992));
 console.log(addToCollection(myCollection, 'Ready to Die', 'The Notorious B.I.G.', 1994));
 console.log(addToCollection(myCollection, 'Achtung Baby', 'U2', 1991));
 console.log(addToCollection(myCollection, 'Ok Computer', 'Radiohead', 1997));
+console.log(addToCollection(myCollection, 'In Utero', 'Nirvana', 1993));
 
 console.log(myCollection);
+
 showCollection(myCollection);
+console.log(findByArtist(myCollection, 'Nirvana'));
+console.log(findByArtist(myCollection, 'U2'));
 
 
 
